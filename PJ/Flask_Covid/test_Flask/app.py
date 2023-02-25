f0ba58a25292bb4adc18f812be632b4d76c3551c
Georgia-MAX-holic <97029416+Georgia-MAX-holic@users.noreply.github.com>
2023-02-25 22:32:56 +0900

    Add files via upload

---
+++ PJ/Flask_Covid/test_Flask/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template , request
+
+import pandas as pd
+
+import joblib
+model = joblib.load('model/COVID_19.pkl')
+    
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/front")
+def front():
+    return render_template('front.html')
+
+@app.route('/blank')
+def blank():
+    return render_template('blank.html')
+
+@app.route('/News')
+def News():
+    return render_template('News.html')
+
+@app.route("/calculate", methods=['POST',"GET"])
+def calculate():
+    return render_template("calculate.html")
+
+@app.route('/calculate/after', methods=['GET',"POST"])
+def home():
+    data1 = request.form["region_p"]
+    data2 = request.form["death"]
+    data3 = request.form["confirm"]
+    
+    arr=pd.DataFrame({
+'지역별인구수':data1,
+"사망자":data2,
+"누적확진":data3,
+}, index=[0])
+    pred = model.predict(arr)
+    data = int(pred)
+    return render_template("cal_after.html" , data=data)
+
+
+
+app.run('127.0.0.1', port=5000)
